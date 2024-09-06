a07ed8bd82b0c13966f742522c2c31151dde9560
Romnoel Edralin Petracorta <petracorta.rmnl@gmail.com>
2024-09-06 21:14:12 +0800

    Update ESP32.java

--- src/ESP32.java
+++ src/ESP32.java
@@ -3,6 +3,49 @@
 // it a great option for people making their houses equipped with 
 // smart home functionalities.
 
+import commands.Command;
+import commands.NoOperationCommand;
+
 public class ESP32 {
-    
+    Command[] runningDevices;
+    Command[] turnedOffDevices;
+    int commandSlots = 4;
+
+    public ESP32() {
+        runningDevices = new Command[commandSlots];
+        turnedOffDevices = new Command[commandSlots];
+
+        for (int i = 0; i < commandSlots; i++) {
+            runningDevices[i] = new NoOperationCommand();
+            turnedOffDevices[i] = new NoOperationCommand();
+        }
+    }
+
+    public void setCommand(int commandSlot, Command turnedOnCommand, Command turnedOffCommand) {
+        if (turnedOnCommand == null || turnedOffCommand == null) {
+            throw new IllegalArgumentException("Command cannot be null");
+        } else {
+            runningDevices[commandSlot] = turnedOnCommand;
+            turnedOffDevices[commandSlot] = turnedOffCommand;
+        }
+    }
+
+    public void activateDevice(int commandSlot) {
+        runningDevices[commandSlot].execute();
+    }
+
+    public void deactivateDevice(int commandSlot) {
+        turnedOffDevices[commandSlot].execute();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\n******ESP32******\n");
+        for (int i = 0; i < commandSlots; i++) {
+            stringBuilder.append("Command:").append(i).append(" - ").append(runningDevices[i].getClass().getSimpleName()).append("(").append(runningDevices.getClass().getSimpleName()).append(")").append(" - ").append(turnedOffDevices[i].getClass().getSimpleName()).append("(").append(turnedOffDevices.getClass().getSimpleName()).append(")").append("\n");
+        }
+
+        return stringBuilder.toString();
+    }
 }
