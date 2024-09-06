f3b1d04b76cd77ae1bfd84b7731d775c61caf374
Romnoel Edralin Petracorta <petracorta.rmnl@gmail.com>
2024-09-06 21:18:24 +0800

    Update NoOperationCommand.java

--- src/commands/NoOperationCommand.java
+++ src/commands/NoOperationCommand.java
@@ -1,5 +1,6 @@
 package commands;
 
+// This class exists just to avoid null reference pointer exception in the ESP32 class. 
 public class NoOperationCommand implements Command {
     @Override
     public void execute() {
