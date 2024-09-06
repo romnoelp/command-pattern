6f4d59417ed54e2582a568d22432463e5ed80caf
Romnoel Petracorta <petracorta.rmnl@gmail.com>
2024-09-06 17:08:25 +0800

    Added the devices, and made the Central Command and the ESP32 device

---
+++ src/ESP32.java
@@ -0,0 +1,8 @@
+// An ESP32 is a microcontroller embedded on a device like an Ardduino.
+// It offers great connectvity options such as bluetooth and WIFI making
+// it a great option for people making their houses equipped with 
+// smart home functionalities.
+
+public class ESP32 {
+    
+}
